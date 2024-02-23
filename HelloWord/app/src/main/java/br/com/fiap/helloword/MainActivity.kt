a2bf3e277facd5bef86714cce96cf0201726bfe9
Henrique Cruz <henriquecc6245@gmail.com>
2024-02-22 22:33:38 -0300

    Primeiro hello world

--- HelloWord/app/src/main/java/br/com/fiap/helloword/MainActivity.kt
+++ HelloWord/app/src/main/java/br/com/fiap/helloword/MainActivity.kt
@@ -1,11 +1,18 @@
 package br.com.fiap.helloword
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Criando um objeto de texto
+        val texto = TextView(this)
+        texto.text = "Hello World"
+
+        setContentView(texto)
     }
 }
